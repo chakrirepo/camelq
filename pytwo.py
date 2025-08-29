65d47b8a703bf6b9066de0cba39462658ec65b78
chakrirepo <varjulavenkat@gmail.com>
2025-08-29 11:22:32 +0530

    updated

---
+++ pytwo.py
@@ -0,0 +1,15 @@
+# Simple Python program
+
+# Print a welcome message
+print("Hello! Welcome to Python with Git and VS Code 🚀")
+
+# Take user input
+name = input("Enter your name: ")
+print("Hi", name, "! Nice to meet you.")
+
+# Do a small calculation
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum_result = num1 + num2
+
+print("The sum of", num1, "and", num2, "is:", sum_result)
